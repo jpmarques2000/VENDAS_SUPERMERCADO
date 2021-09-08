eb66648183e292d4daba4dfc60947b036ddeb45b
Joao Paulo Marques <jpmarques2000@hotmail.com>
2021-09-08 18:46:03 -0300

    Correcao

--- VENDAS_SUPERMERCADO/VENDAS_SUPERMERCADO/Models/NavigationService.cs
+++ VENDAS_SUPERMERCADO/VENDAS_SUPERMERCADO/Models/NavigationService.cs
@@ -29,7 +29,7 @@ namespace VENDAS_SUPERMERCADO.Views
 
         public async Task NavigateToMyCart()
         {
-            App.Current.MainPage.Navigation.PushAsync(new MyCartPage());
+            await App.Current.MainPage.Navigation.PushAsync(new MyCartPage());
         }
 
         public async Task NavigateToMyOrder()
